4fc8b9e63211591cd71da2fcdfbeff1816a1a875
Maciej Szulik <maszulik@redhat.com>
2017-12-14 14:15:32 +0100

    Add origin

--- cmd/tester/main.go
+++ cmd/tester/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/golang/glog"
 
+	apps "github.com/openshift/client-go/apps/clientset/versioned"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -30,7 +32,8 @@ func main() {
 	config.UserAgent = "api-killer/v1.0.0"
 	config.RateLimiter = flowcontrol.NewFakeNeverRateLimiter()
 
-	clientset, err := kubernetes.NewForConfig(config)
+	// clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := apps.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -38,7 +41,8 @@ func main() {
 	i := 0
 	for {
 		glog.Infof("Starting %d batch...", i)
-		setupSecretWatches(clientset, 200)
+		// setupSecretWatches(clientset, 200)
+		setupDeploymentWatches(clientset, 200)
 		time.Sleep(50 * time.Millisecond)
 		i = i + 1
 	}
@@ -56,6 +60,23 @@ func setupSecretWatches(clientset *kubernetes.Clientset, limit int) {
 	for i := 0; i < limit; i++ {
 		go func() {
 			watch, err := clientset.CoreV1().Secrets("").Watch(metav1.ListOptions{})
+			if err != nil {
+				glog.Errorf("Error setting watch: %v", err)
+			}
+			select {
+			case event := <-watch.ResultChan():
+				glog.V(3).Infof("Got %v", event)
+			case <-time.After(20 * time.Minute):
+				watch.Stop()
+			}
+		}()
+	}
+}
+
+func setupDeploymentWatches(clientset *apps.Clientset, limit int) {
+	for i := 0; i < limit; i++ {
+		go func() {
+			watch, err := clientset.AppsV1().DeploymentConfigs("").Watch(metav1.ListOptions{})
 			if err != nil {
 				glog.Errorf("Error setting watch: %v", err)
 			}
